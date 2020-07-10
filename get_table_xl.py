560e3b180a0fc91b19c73e970ae61c4de687c502
theBar10 <37342344+theBar10@users.noreply.github.com>
2020-07-10 14:48:03 -0500

    Dynamic column name change
    Added in a variable to pull the key for type of data (one of the columns) and for the unit of measure (uom is the other dynamic column name change.) 
    This gave the ability to keep the different types of data that is being pulled separate for when they are used for exploratory data analysis.

--- get_table_xl.py
+++ get_table_xl.py
@@ -13,87 +13,78 @@ Commented out print(wb.get_sheet_names()) to only use when I need to check the s
 
 #selecting the tab from the excel file that I want to work in; tab name is a string; keep it in ('')
 sheet = wb.get_sheet_by_name('Actual Drivers')
+#the tables in the sheets do not align as well as I thought they would, should make a copy of this script and change the sheet and values to match
 
-myDict = {}
-
-"""
-myDict is set to a blank dictionary in prep for the series of list that the next section will add.
+"""myDict is set to a blank dictionary in prep for the series of list that the next section will add.
 
 Using the below outer for and inner for loops you can work through an array of cells in Excel to get their values.
 The outer for loop (i) sets the number of rows to get the values from.
 The inner for loop (n) sets the number of columns to get the values from.
 With each for loop the first integer in the range is the cell number to start with, it must start with 1 or higher (zero doesn't equal one).
-The second integer in the range for the loops is the number of rows or columns that are to be looped through.
-"""
+The second integer in the range for the loops is the number of rows or columns that are to be looped through."""
 
-table_name = "Reissues"  #don't forget to change the soved file name
-table_start = 6
-table_end = 11
+table_dict = \
+{"Reissues":{"first":6, "last":11, "uom":"dollars", "group_type":"type"}, \
+ "Yield_Pounds":{"first":16, "last":22, "uom":"pounds", "group_type":"department"}, \
+ "Window_Units":{"first":61, "last":70, "uom":"units", "group_type":"department"}, \
+ "Warehouse_Pieces":{"first":95, "last":96, "uom":"sticks", "group_type":"department"}}
 
 
-for i in range (5, 6): #for the fixed row of dates
-    for n in range (2, 74): 
-        data = ([])
-        data.append(sheet.cell(row = i, column = n).value)
-        myDict.setdefault(n, []).append(data)
+
+for table in table_dict:
+    myDict = {}
+    for i in range (5, 6): #for the fixed row of dates
+            for n in range (2, 74): 
+                data = ([])
+                data.append(sheet.cell(row = i, column = n).value)
+                myDict.setdefault(n, []).append(data)
 
         
-for i in range (table_start, table_end): 
-    for n in range (2, 74): 
-        data = ([])
-        data.append(sheet.cell(row = i, column = n).value)
-        myDict.setdefault(n, []).append(data)
+    for i in range (table_dict[table]['first'], table_dict[table]['last']):
+        for n in range (2, 74): 
+            data = ([])
+            data.append(sheet.cell(row = i, column = n).value)
+            myDict.setdefault(n, []).append(data)
 
-delete = (3,4,5,6,7)
+    delete = (3,4,5,6,7)
 
-for d in delete:
-    del myDict[d]
-""" 
-Removing some unwanted noise from the dataset.
-Data contains calculations for month end and year end. 
-The 'delete' assignment was made instead of looping through the range incase other columns need to be added for other sheets.
-"""
-
-df = pd.DataFrame.from_dict(myDict)
-
-"""
-Pandas (pd) are easier to work with when reshaping data for analysis.
-I wanted to use the pandas dataframe to wrangle the data.
-"""
-
-for r in df:
-    df[r] = df[r].str.get(0)
-""""
-Since I created the dataframe out of list of list I was stuck with square brackets
-This for loop gets the string of list, effectively removing the square brackets from the dataframe.
-"""
-
-
-df=df.set_index([2]).stack().unstack([0]).reset_index()
-df=df.rename(columns={None:'Dates'})
-df=df.melt(id_vars = ['index', 'Dates'])
-df=df.rename(columns={2:table_name})
-"""
-I am sure there is a much cleaner way to reshape a long data set that has a column and row that need to be indexed.
-Either way, I modified the data into a long form with stack, then I unstacked what was the first row, then I used those
-two columns to 'melt' the data into the shape that I was trying to work with.
-""" 
-
-df=df[df.Dates != 'MTD']
-df=df[df.Dates != 'YTD']
-"""
-The data set had this reoccurring month to date (MTD) 
-and year to date (YTD) calculation in it that was not needed. 
-The doesn't equal removes or filters out that calculation.
-"""
-
-print(df.head())
-
-df.to_csv(r'C:\Users\tbarten\Desktop\Projects\Cost_Productivity\PyRun\Reissue2020.csv')
-
-"""
-I like to display the top rows of the of the dataframe (i.e. print(df.head())) so that I have a heads up
-of what the data looks like before it is sent to the file. It won't change the file or stop the process if it isn't correct. 
-The df.to_csv then writes it to the sheet that I want to work with. For now, I will send it to a new sheet each time
-that I run this code. I will also change the initial for range with i & n to grab the multiple tables form this sheet. 
-"""
+    for d in delete:
+        del myDict[d]
+    
+    """Pandas (pd) are easier to work with when reshaping data for analysis.
+    I wanted to use the pandas dataframe to wrangle the data."""    
+    df = pd.DataFrame.from_dict(myDict)
+    
+    """"Since I created the dataframe out of list of list I was stuck with square brackets
+    This for loop gets the string of list, effectively removing the square brackets from the dataframe."""
+    for r in df:
+        df[r] = df[r].str.get(0)
+    
+ 
+    """I am sure there is a much cleaner way to reshape a long data set that has a column and row that need to be indexed.
+    Either way, I modified the data into a long form with stack, then I unstacked what was the first row, then I used those
+    two columns to 'melt' the data into the shape that I was trying to work with."""     
+    df=df.set_index([2]).stack().unstack([0]).reset_index()
+    df=df.rename(columns={None:'Dates'})
+    df=df.melt(id_vars = ['index', 'Dates'])
+    
+    """Added in some dynamic column name changes. 
+    This helps drill with the two common variables of this dataset."""
+    group = table_dict[table]['group_type']
+    uom = table_dict[table]['uom']
+    
+    df=df.rename(columns={2:group})
+    df=df.rename(columns={'value':uom})
+    
+    """The data set had this reoccurring month to date (MTD) 
+    and year to date (YTD) calculation in it that was not needed. 
+    The doesn't equal removes or filters out that calculation."""
+    df=df[df.Dates != 'MTD']
+    df=df[df.Dates != 'YTD']
+    
+    df.fillna(0) #making null values zero
+    
+    #print(df.info())
+    
+    """save the reshaped dataframe and changed the name to dynamically align with dict key"""
+    df.to_csv(r'C:\Users\tbarten\Desktop\Projects\Cost_Productivity\PyRun\actual {0}2020.csv'.format(table))
